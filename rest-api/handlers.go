4f101aad8b795300c48516150b833d1f758e365a
mattfan00 <kungfumo@gmail.com>
2020-09-09 13:34:20 -0400

    send as json

--- rest-api/handlers.go
+++ rest-api/handlers.go
@@ -21,9 +21,12 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	newEncoder := json.NewEncoder(w)
-	// newEncoder.SetIndent("", "    ")
-	newEncoder.Encode(todos)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
