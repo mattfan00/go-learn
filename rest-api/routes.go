648a0641ee87b6666387fb98c7a2d912c5ad6eb7
mattfan00 <kungfumo@gmail.com>
2020-09-09 12:52:05 -0400

    added logging

--- rest-api/routes.go
+++ rest-api/routes.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	"time"
 )
+
+func Logger(fn http.HandlerFunc, name string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("%s\t%s\t%s\t%s\t", r.Method, r.RequestURI, name, time.Since(start))
+
+		fn(w, r)
+	}
+}
 
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
 	for _, route := range routes {
+		handler := Logger(route.HandlerFunc, route.Name)
+
 		router.
 			Path(route.Path).
 			Methods(route.Method).
 			Name(route.Name).
-			HandlerFunc(route.HandlerFunc)
+			HandlerFunc(handler)
 	}
 
 	return router
